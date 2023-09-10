ab474048078fff11b70fd689a24db4a20263d9e8
DANI BACHINI <dani_costa90@yahoo.com.br>
2023-09-10 11:52:37 +0200

    layout projects done + testing .env not pushing

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,30 +3,29 @@ import { dictionary } from '@/content';
 
 export default function ({ params }: { params: { lang: string } }) {
   return (
-    <main className='flex flex-wrap flex-grow justify-center md:justify-start gap-4 md:gap-6'>
-      {/* <div className='min-w-full md:min-w-0 md:w-64'> */}
-      <div className='mx-auto md:min-w-0 md:w-64 bg-slate-400'>
+    <main className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 justify-center'>
+      <div className='h-64 bg-slate-400'>
         <p>column 1</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
       </div>
-      <div className='mx-auto md:min-w-0 md:w-64 bg-slate-400'>
+      <div className='h-64 bg-slate-400'>
         <p>column 2</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
       </div>
-      <div className='mx-auto md:min-w-0 md:w-64 bg-slate-400'>
+      <div className='h-64 bg-slate-400'>
         <p>column 3</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
       </div>
-      <div className='mx-auto md:min-w-0 md:w-64 bg-slate-400'>
-        <p>column 2</p>
+      <div className='h-64 bg-slate-400'>
+        <p>column 4</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
       </div>
-      <div className='mx-auto md:min-w-0 md:w-64 bg-slate-400'>
-        <p>column 3</p>
+      <div className='h-64 bg-slate-400'>
+        <p>column 5</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
         <p>{dictionary[params.lang]?.homeContent}</p>
       </div>
