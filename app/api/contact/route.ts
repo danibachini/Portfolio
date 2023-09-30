7cb5a85838d5f325a0df94e53761c60056bd3ee5
DANI BACHINI <dani_costa90@yahoo.com.br>
2023-09-30 10:05:07 +0200

    editing next.config.js

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,6 @@
 
-import { error } from "console";
-import { NextResponse } from "next/server";
+// import { error } from "console";
+// import { NextResponse } from "next/server";
 
 export async function POST (req : Request) {
 
--- next.config.js
+++ next.config.js
@@ -2,14 +2,22 @@
 const nextConfig = {}
 
 module.exports = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'thumbnails-portfolio.s3.eu-west-3.amazonaws.com',
-            port: '',
-            pathname: '**',
-          },
-        ],
-      },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    EMAIL: process.env.EMAIL,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+
+  },
+
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'thumbnails-portfolio.s3.eu-west-3.amazonaws.com',
+          port: '',
+          pathname: '**',
+        },
+      ],
+    },
+
 }, nextConfig
