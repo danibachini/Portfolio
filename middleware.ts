a4858a485a170821491b2219d54787107a9932c7
DANI BACHINI <dani_costa90@yahoo.com.br>
2023-10-01 16:42:00 +0200

    installing some packages + updates

--- middleware.ts
+++ middleware.ts
@@ -43,14 +43,13 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { i18n } from './i18n.config'
 import { match as matchLocale } from '@formatjs/intl-localematcher'
-// import Negotiator from 'negotiator'
-var Negotiator = require('negotiator')
+import Negotiator from 'negotiator'
 
 function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {}
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
-  // @ts-ignore locales are readonly
+  // @ts-ignore locales are readonl
   const locales: string[] = i18n.locales
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
 
--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {}
 
 module.exports = {
+  // i18n: {
+  //   // These are all the locales you want to support in
+  //   // your application
+  //   locales: ['en',  'fr'],
+  //   // This is the default locale you want to be used when visiting
+  //   // a non-locale prefixed path e.g. `/hello`
+  //   defaultLocale: 'en',
+  //   // This is a list of locale domains and the default locale they
+  //   // should handle (these are only required when setting up domain routing)
+  //   domains: [
+  //     {
+  //       domain: 'danibachini.com',
+  //       defaultLocale: 'en',
+  //       // other locales that should be handled on this domain
+  //       locales: ['fr'],
+  //     },
+  //   ],
+  // },
 
   images: {
     remotePatterns: [
